a5313b45c5799f4b6b70b890273bd49c6afc99ea
Karthik Jagadeesh <kjag@stanford.edu>
2018-08-09 14:09:23 -0700

    adding setup scripts

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+ 
+setup(
+    name='Phrank',    # This is the name of your PyPI-package.
+    version='1.0',                          # Update the version number for new releases
+    scripts=['phrank']                  # The name of your scipt, and also the command you'll be using for calling it
+)
