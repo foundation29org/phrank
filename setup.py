63e4e258fffe64d783feb5ff64c5e79249f459a9
Karthik Jagadeesh <kjag@stanford.edu>
2018-08-09 14:32:30 -0700

    updates to setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
- 
-setup(
-    name='Phrank',    # This is the name of your PyPI-package.
-    version='1.0',                          # Update the version number for new releases
-    scripts=['phrank']                  # The name of your scipt, and also the command you'll be using for calling it
-)
+
+setup(name='phrank',
+      version='1.0',
+      description='Measuring phenotype set similarity',
+      url='https://bitbucket.org/bejerano/phrank/',
+      author='Bejerano Lab',
+      author_email='kjag@cs.stanford.edu',
+      license='MIT',
+      packages=['phrank'],
+      zip_safe=False)
